87ef2b18fdbb9657dfb013c56049df0e487680fb
Andrew Collier <andrew@fathomdata.dev>
2024-02-21 16:57:10 +0000

    feat: Version selector is responsive

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,6 +8,17 @@ import { StaticQuery, graphql } from "gatsby"
 // CAN WE DELETE ../util/fetchVersions???
 
 class DefaultLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedVersion: ''
+    };
+  }
+
+  handleVersionChange = (event) => {
+    this.setState({ selectedVersion: event.target.value });
+  };
+
   render() {
     return (
       <StaticQuery
@@ -28,16 +39,18 @@ class DefaultLayout extends React.Component {
         }
         `}
         render={data => {
-          const versions = Array.from(new Set(data.allAsciidoc.nodes.map(node => parseFloat(node.fields.version))));
+          const versions = Array.from(new Set(data.allAsciidoc.nodes.map(node => parseFloat(node.fields.version)))).sort((a, b) => b - a);
+          const selectedVersion = this.state.selectedVersion ? this.state.selectedVersion : Math.max(...versions);
           return (
           <div className="prose lg:prose-lg my-0 max-w-full">
+            {selectedVersion}
             <div className="p-3 text-center bg-black">
               <h2 id="heading" className="text-white">{data.site.siteMetadata.title}</h2>
             </div>
             <div className="flex">
               <div id="nav-left" className="flex-1" style={{ flex: '0 0 20%' }}>
                 <label htmlFor="version-selector">Select Version:</label>
-                <select id="version-selector">
+                <select id="version-selector" value={selectedVersion} onChange={this.handleVersionChange}>
                   {versions.map((version, index) => (
                     <option key={index} value={version}>{version}</option>
                   ))}
