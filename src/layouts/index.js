09d59d72df79c3ae4d109c53135fa037081695dd
Andrew Collier <andrew@fathomdata.dev>
2024-02-21 16:31:32 +0000

    feat: Add version selector

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -31,12 +31,17 @@ class DefaultLayout extends React.Component {
           const versions = Array.from(new Set(data.allAsciidoc.nodes.map(node => parseFloat(node.fields.version))));
           return (
           <div className="prose lg:prose-lg my-0 max-w-full">
-            {JSON.stringify(versions)}
             <div className="p-3 text-center bg-black">
               <h2 id="heading" className="text-white">{data.site.siteMetadata.title}</h2>
             </div>
             <div className="flex">
-              <div className="flex-1" style={{ flex: '0 0 20%' }}>
+              <div id="nav-left" className="flex-1" style={{ flex: '0 0 20%' }}>
+                <label htmlFor="version-selector">Select Version:</label>
+                <select id="version-selector">
+                  {versions.map((version, index) => (
+                    <option key={index} value={version}>{version}</option>
+                  ))}
+                </select>
               </div>
               <div className="flex-1" style={{ flex: '0 0 80%' }}>
                 <div className="pt-6 max-w-3xl">
