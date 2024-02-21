ed33e2bf5846f6e21963b0e26bd706da0d806c16
Andrew Collier <andrew@fathomdata.dev>
2024-02-21 17:55:54 +0000

    test: Change to function

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
 // CAN WE DELETE ../util/fetchVersions???
 // CAN WE DELETE ../util/fetchVersions???
@@ -7,69 +6,22 @@ import { StaticQuery, graphql } from "gatsby"
 // CAN WE DELETE ../util/fetchVersions???
 // CAN WE DELETE ../util/fetchVersions???
 
-class DefaultLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedVersion: ''
-    };
-  }
+// THE PLAN: WHEN THE SELECTED VERSION CHANGES THEN NAVIGATE TO NEW PAGE.
+// THE PLAN: WHEN THE SELECTED VERSION CHANGES THEN NAVIGATE TO NEW PAGE.
+// THE PLAN: WHEN THE SELECTED VERSION CHANGES THEN NAVIGATE TO NEW PAGE.
+// THE PLAN: WHEN THE SELECTED VERSION CHANGES THEN NAVIGATE TO NEW PAGE.
 
-  handleVersionChange = (event) => {
-    this.setState({ selectedVersion: event.target.value });
-  };
+const DefaultLayout = ({ children }) => {
+  return (
+    <div className="prose lg:prose-lg my-0 max-w-full">
+      <div className="p-3 text-center bg-black">
+        <h2 id="heading" className="text-white">Domino Split Documentation</h2>
+      </div>
+      <div className="pt-6 mx-auto max-w-3xl">
+        {children}
+      </div>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <StaticQuery
-        query={graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          allAsciidoc {
-            nodes {
-                fields {
-                    version
-                }
-            }
-          }
-        }
-        `}
-        render={data => {
-          const versions = Array.from(new Set(data.allAsciidoc.nodes.map(node => parseFloat(node.fields.version)))).sort((a, b) => b - a);
-          const selectedVersion = this.state.selectedVersion ? this.state.selectedVersion : Math.max(...versions);
-          return (
-          <div className="prose lg:prose-lg my-0 max-w-full">
-            AAA {selectedVersion} BBB
-            <div className="p-3 text-center bg-black">
-              <h2 id="heading" className="text-white">{data.site.siteMetadata.title}</h2>
-            </div>
-            <div className="flex">
-              <div id="nav-left" className="flex-1" style={{ flex: '0 0 20%' }}>
-                <label htmlFor="version-selector">Select Version:</label>
-                <select id="version-selector" value={selectedVersion} onChange={this.handleVersionChange}>
-                  {versions.map((version, index) => (
-                    <option key={index} value={version}>{version}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="flex-1" style={{ flex: '0 0 80%' }}>
-                <div className="pt-6 max-w-3xl">
-                  {React.Children.map(this.props.children, child => {
-                    return React.cloneElement(child, { selectedVersion: selectedVersion });
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-          );
-        }}
-      />
-    )
-  }
-}
-
-export default DefaultLayout
+export default DefaultLayout;
