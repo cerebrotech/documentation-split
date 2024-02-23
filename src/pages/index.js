028008c28944e069e3a79cf46641b914060646f9
Andrew Collier <andrew@fathomdata.dev>
2024-02-23 13:56:42 +0000

    fix: Remove explicit links on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,12 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
 
 import Layout from "../layouts";
 
 function IndexPage(props) {
   return (
     <Layout>
-      <div>
-        <ul>
-          <li><Link to="/5.9/user-guide/">User Guide</Link></li>
-          <li><Link to="/5.9/admin-guide/">Admin Guide</Link></li>
-          <li><Link to="/5.9/api-guide/">API Guide</Link></li>
-        </ul>
-      </div>
     </Layout>
   );
 }
 
 export default IndexPage;
-
-export const pageQuery = graphql`
-  query {
-    allAsciidoc {
-      nodes {
-        id
-        html
-        pageAttributes {
-          title
-        }
-        fields {
-          slug
-          version
-        }
-      }
-    }
-  }
-`;
